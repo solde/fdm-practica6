77adb3b79bad3a31d27cd744d866d69dbf51a96c
solde <david.soldevila.puigbi@est.fib.upc.edu>
2021-05-04 16:48:51 +0200

    update

--- Practica6.py
+++ Practica6.py
@@ -1,8 +1,10 @@
-from sympy import *
+import sympy as sym
 import numpy as np
 import math
+import matplotlib.pyplot as plt
 
-x = Symbol('x')
+x = sym.Symbol('x')
+n = sym.Symbol('n')
 
 # Constants
 h = 6.62607004 * 10**-34
@@ -11,13 +13,36 @@ a = 1
 k = 9.9 * 10**-21
 w = 3.1 * 10**5
 
-# Wave equations
-Y0 = (a/np.pi**1/2)**1/2*np.e**((-a**2*x**2)/2)
-Y1 = (a/np.pi**1/2)**1/2*2*a*np.e**((-a**2*x**2)/2)
-Y2 = (a/np.pi**1/2)**1/2*(4*a**2*x**2-2)*np.e**((-a**2*x**2)/2)
-Y3 = (a/np.pi**1/2)**1/2*(8*a**3*x**3-12*a*x)*np.e**((-a**2*x**2)/2)
+Y = np.empty(4, dtype=sym.core.mul.Mul)
+d2Y = np.empty(4, dtype=sym.core.mul.Mul)
 
-print(diff(Y0, x))
-print(diff(Y1, x))
-print(diff(Y2, x))
-print(diff(Y3, x))
+# Wave equations
+Y[0] = (a/np.pi**1/2)**1/2*np.e**((-a**2*x**2)/2)
+Y[1] = (a/np.pi**1/2)**1/2*2*a*np.e**((-a**2*x**2)/2)
+Y[2] = (a/np.pi**1/2)**1/2*(4*a**2*x**2-2)*np.e**((-a**2*x**2)/2)
+Y[3] = (a/np.pi**1/2)**1/2*(8*a**3*x**3-12*a*x)*np.e**((-a**2*x**2)/2)
+
+d2Y[0] = sym.diff(sym.diff(Y[0], x) , x)
+d2Y[1] = sym.diff(sym.diff(Y[1], x) , x)
+d2Y[2] = sym.diff(sym.diff(Y[2], x) , x)
+d2Y[3] = sym.diff(sym.diff(Y[3], x) , x)
+
+# Gràfica de l'energia
+print("Calcul de la gràfica de l'energia")
+
+print("Entrar el nombre de n que es volen calcular: ")
+iterations = int(input()) # number of energies that will be calculated
+
+for i in range(0, iterations):
+    n = float(input())
+
+    En = (n+0.5)*(h/(2*np.pi))*w
+
+# Provabilitat
+
+P = np.empty([4, 20])
+
+for j in range(-10, 10):
+    for i in range(0, 4):
+        P[i, j] = d2Y[i].subs(x, j)
+        P[i, j] = P[i, j]**2
