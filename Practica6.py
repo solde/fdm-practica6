a6fba730f49c0735c1f08ed71d68adb0eb5933de
solde <david.soldevila.puigbi@est.fib.upc.edu>
2021-05-25 14:21:36 +0200

    update

--- Practica6.py
+++ Practica6.py
@@ -2,80 +2,125 @@ import sympy as sym
 import numpy as np
 import math
 import matplotlib.pyplot as plt
-import threading
 
-x = sym.Symbol('x')
-n = sym.Symbol('n')
+sym.init_printing(use_unicode=True)
+
+x = sym.Symbol('x', nonzero=True, real=True )
+n = sym.Symbol('n', nonzero=True)
 
 # Constants
-h = 6.62607004 * 10**-34
-m = 9.10938356 * 10**-31
-a = 1 
-k = 9.9 * 10**-21
-w = 3.1 * 10**5
+h = 6.62607004 * 10**-34    #J*s
+m = 9.10938356 * 10**-31    #Kg
+a = 1                       #No te unitats
+k = 9.9 * 10**-21           #N/m
+w = math.sqrt(k/m)          #rad/s
+q = 1.602177 * 10**-19      #Q
 
 Y = np.empty(4, dtype=sym.core.mul.Mul)
-d2Y = np.empty(4, dtype=sym.core.mul.Mul)
 
 # Wave equations
-Y[0] = (a/(np.pi)**1/2)**1/2*np.e**((-a**2*x**2)/2)
-Y[1] = ((a/(2*((np.pi)**1/2)))**1/2)*2*x*a*np.e**((-a**2*x**2)/2)
-Y[2] = (a/(8*((np.pi)**1/2)))**1/2*((4*a**2*x**2)-2)*np.e**((-a**2*x**2)/2)
-Y[3] = (a/(48*((np.pi)**1/2)))**1/2*(8*a**3*x**3-12*a*x)*np.e**((-a**2*x**2)/2)
-
-d2Y[0] = sym.diff(sym.diff(Y[0], x) , x)
-d2Y[1] = sym.diff(sym.diff(Y[1], x) , x)
-d2Y[2] = sym.diff(sym.diff(Y[2], x) , x)
-d2Y[3] = sym.diff(sym.diff(Y[3], x) , x)
+Y[0] = sym.simplify((a/(np.pi)**1/2)**1/2*np.e**((-a**2*x**2)/2))
+Y[1] = sym.simplify(((a/(2*((np.pi)**1/2)))**1/2)*2*x*a*np.e**((-a**2*x**2)/2))
+Y[2] = sym.simplify((a/(8*((np.pi)**1/2)))**1/2*((4*a**2*x**2)-2)*np.e**((-a**2*x**2)/2))
+Y[3] = sym.simplify((a/(48*((np.pi)**1/2)))**1/2*(8*a**3*x**3-12*a*x)*np.e**((-a**2*x**2)/2))
 
 # Gràfica de l'energia
 print("Calcul de la gràfica de l'energia")
 
-print("Entrar el nombre de n que es volen calcular: ")
-iterations = int(input()) # number of energies that will be calculated
+Ej = []
+Eev = []
+n = []
+aux = input("Entre el valor de n pel que vulguis calcular l'energia (escriu stop per acabar): ")
 
-for i in range(0, iterations):
-    n = float(input())
+while aux != "stop":
+    n.append(float(aux))
 
-    En = (n+0.5)*(h/(2*np.pi))*w
+    Ej.append((n[-1]+0.5)*(h/(2*np.pi))*w)
+    Eev.append(Ej[-1]/q)
+    
+    print("Per la n " + str(n[-1]) + " la seva energia és " + str(Ej[-1]) + "J o " + str(Eev[-1]) + "eV.")
 
-# Probabilitat
-def prob(int i):
-    print("Calcul de la probabilitat")
+    aux = input("Entre el valor de n pel que vulguis calcular l'energia (escriu stop per acabar): ")
 
-    min = -1
-    max = 1
-    interval = 0.001
-    plotRange = np.arange(min, max, interval)
+plt.plot(n, Eev, 'o')
+plt.title("Energia")
+plt.xlabel("n")
+plt.ylabel("Energia (eV)")
+plt.draw()
+plt.savefig("Energia.png")
+plt.clf()
 
-    P = np.empty([4, int((max-min)/interval)])
+# Gràfica de la funció d'ona
 
-    it = 0
-    for j in plotRange:
-        aux = d2Y[i].subs(x, it)
-        P[i, it] = aux**2
-        it += 1
+min = -5
+max = 5
+interval = 0.01
+plotRange = np.arange(min, max, interval)
 
-    print("Generant els .csv")
+P = np.empty([4, plotRange.shape[0]])
 
+print("Calculant l'ona")
 
-    count = min
-    f = open("Ona"+str(j+1)+".csv", "w")
-    for j in P[i]:
-        f.write(str(count) + ", " + str(j) + "\n")
-        count += interval
-    f.close()
+it = 0
+for j in plotRange:
+    for i in range(0, 4):
+        P[i, it] = Y[i].subs(x, j)
+    it += 1
 
-    print("Generant els plots")
+print("Generant les gràfiques de la funció d'ona")
 
-    plt.plot(plotRange, P[i])
-    plt.title("Probabilitat de la Ψ" + str(j+1))
+for j in range(0, 4):
+    plt.plot(plotRange, P[j])
+    plt.title("Funció d'ona Ψ" + str(j))
+    plt.xlabel("Posició (m)")
+    plt.ylabel("Valor de l'Energia (J)")
+    plt.draw()
+    plt.savefig("OnaN"+str(j)+".png")
+    plt.clf()
+
+# Densitat de probabilitat
+
+min = -5
+max = 5
+interval = 0.01
+plotRange = np.arange(min, max, interval)
+
+P = np.empty([4, plotRange.shape[0]])
+
+print("Calculant la probabilitat")
+
+it = 0
+for j in plotRange:
+    for i in range(0, 4):
+        P[i, it] = (Y[i].subs(x, j))**2
+    it += 1
+
+print("Generant les grafiques")
+
+for j in range(0, 4):
+    plt.plot(plotRange, P[j])
+    plt.title("Probabilitat de la Ψ" + str(j))
     plt.xlabel("Posició (m)")
     plt.ylabel("Probabilitat (%)")
     plt.draw()
-    plt.savefig("Grafic"+str(i+1)+".png")
+    plt.savefig("DPOnaN"+str(j)+".png")
+    plt.clf()
 
-for i in range(0,4):
-    thr = threading.Thread(target=prob, args=(i))
-    thr.start()
-    
+# Probabilitat
+
+aux = input("Entra la n(0-3) (excriu stop per acabar): ")
+
+while aux != "stop":
+
+    nwave = int(aux)-1
+    a = float(input("Posició x0: "))
+    b = float(input("Posició x1: "))
+
+    Y2 = sym.parse_expr(str( sym.simplify( Y[nwave] ) )+"*"+str( sym.simplify( ( Y[nwave] ) ) ))
+    Y2 = sym.simplify(Y2)
+    iY2 = sym.integrate(Y[nwave], (x, a ,b))
+    iY2 = sym.simplify(iY2)
+    iY2d = iY2.doit()
+    iY2v = iY2d.evalf()
+    print("La probabilitat de trobar la particula en X0 = " + str(a) + " i el punt X1 " + str(b) + " amb n = " + str(nwave+1) + " és " + str(abs(iY2v)*100) + "%.")
+    aux = input("Entra la n(0-3): ")
